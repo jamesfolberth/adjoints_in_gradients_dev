ef3c5a1930c6f86e47de932ec6fed8c9aafb5546
James Folberth <jamesfolberth@gmail.com>
2016-02-28 19:35:09 -0700

    Commit some changes to the paper

--- ieee_spm/figures/trim.sh
+++ ieee_spm/figures/trim.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-pdfcrop -margins 10 cameraman_rec_100_bior4.4_zpd.pdf cameraman_rec_100_bior4.4_zpd_trim.pdf
-pdfcrop -margins 10 cameraman_rec_200_bior4.4_zpd.pdf cameraman_rec_200_bior4.4_zpd_trim.pdf
-pdfcrop -margins 10 cameraman_rec_100_db1_zpd.pdf cameraman_rec_100_db1_zpd_trim.pdf
-pdfcrop -margins 10 cameraman_rec_200_db1_zpd.pdf cameraman_rec_200_db1_zpd_trim.pdf
+pdfcrop -margins 0 cameraman_rec_100_bior4.4_zpd.pdf cameraman_rec_100_bior4.4_zpd_trim.pdf
+pdfcrop -margins 0 cameraman_rec_200_bior4.4_zpd.pdf cameraman_rec_200_bior4.4_zpd_trim.pdf
+pdfcrop -margins 0 cameraman_rec_100_db1_zpd.pdf cameraman_rec_100_db1_zpd_trim.pdf
+pdfcrop -margins 0 cameraman_rec_200_db1_zpd.pdf cameraman_rec_200_db1_zpd_trim.pdf
 
-pdfcrop -margins 10 cameraman_rec_100_bior4.4_sym.pdf cameraman_rec_100_bior4.4_sym_trim.pdf
-pdfcrop -margins 10 cameraman_rec_200_bior4.4_sym.pdf cameraman_rec_200_bior4.4_sym_trim.pdf
-pdfcrop -margins 10 cameraman_rec_100_db1_sym.pdf cameraman_rec_100_db1_sym_trim.pdf
-pdfcrop -margins 10 cameraman_rec_200_db1_sym.pdf cameraman_rec_200_db1_sym_trim.pdf
+pdfcrop -margins 0 cameraman_rec_100_bior4.4_sym.pdf cameraman_rec_100_bior4.4_sym_trim.pdf
+pdfcrop -margins 0 cameraman_rec_200_bior4.4_sym.pdf cameraman_rec_200_bior4.4_sym_trim.pdf
+pdfcrop -margins 0 cameraman_rec_100_db1_sym.pdf cameraman_rec_100_db1_sym_trim.pdf
+pdfcrop -margins 0 cameraman_rec_200_db1_sym.pdf cameraman_rec_200_db1_sym_trim.pdf
 
-pdfcrop -margins 10 cameraman_rec_200_sym_db1-bior4.4.pdf cameraman_rec_200_sym_db1-bior4.4_trim.pdf
+pdfcrop -margins 0 cameraman_rec_200_sym_db1-bior4.4.pdf cameraman_rec_200_sym_db1-bior4.4_trim.pdf
 
