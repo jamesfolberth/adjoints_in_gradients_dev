786321cecfd247332a34ed11d1382a2117514e15
James Folberth <jamesfolberth@gmail.com>
2016-03-03 15:39:27 -0700

    First draft of slides; okay BCE for certain params

--- siam_framsc_slides/figures/trim.sh
+++ siam_framsc_slides/figures/trim.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
-pdfcrop -margins 10 kmeans_22130988_true_labels.pdf kmeans_22130988_true_labels_trim.pdf
-pdfcrop -margins 10 kmeans_22130988_hard_em!.pdf kmeans_22130988_hard_em!_trim.pdf
+pdfcrop -margins 10 bce_rec_01_h.pdf bce_rec_01_h_trim.pdf
+pdfcrop -margins 10 bce_rec_01_s.pdf bce_rec_01_s_trim.pdf
+pdfcrop -margins 10 bce_rec_01_x.pdf bce_rec_01_x_trim.pdf
+
+pdfcrop -margins 10 bce_rec_02_h.pdf bce_rec_02_h_trim.pdf
+pdfcrop -margins 10 bce_rec_02_s.pdf bce_rec_02_s_trim.pdf
+pdfcrop -margins 10 bce_rec_02_x.pdf bce_rec_02_x_trim.pdf
+
+pdfcrop -margins 10 bce_rec_03_h.pdf bce_rec_03_h_trim.pdf
+pdfcrop -margins 10 bce_rec_03_s.pdf bce_rec_03_s_trim.pdf
+pdfcrop -margins 10 bce_rec_03_x.pdf bce_rec_03_x_trim.pdf
 
