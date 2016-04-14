9560ba51cbbfbdd6d7a0e7a33685eac0bd9776dd
James Folberth <jamesfolberth@gmail.com>
2016-04-14 15:45:06 -0600

    More revisions

--- bce_unconstrained_formulation/recovery_plots/trim.sh
+++ bce_unconstrained_formulation/recovery_plots/trim.sh
@@ -11,3 +11,7 @@ pdfcrop -margins 10 bce_rec_003_h.pdf bce_rec_003_h_trim.pdf
 pdfcrop -margins 10 bce_rec_003_s.pdf bce_rec_003_s_trim.pdf
 pdfcrop -margins 10 bce_rec_003_x.pdf bce_rec_003_x_trim.pdf
 
+pdfcrop -margins 10 bce_rec_004_h.pdf bce_rec_004_h_trim.pdf
+pdfcrop -margins 10 bce_rec_004_s.pdf bce_rec_004_s_trim.pdf
+pdfcrop -margins 10 bce_rec_004_x.pdf bce_rec_004_x_trim.pdf
+
